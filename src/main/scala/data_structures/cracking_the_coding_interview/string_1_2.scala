8d122f239a4f44e2d23edd2d6556b4d80c4344f2
rimisahu <rimi.m.sahu@gmail.com>
2019-10-26 18:04:07 -0400

    string reversing edge case. code cleanup

--- src/main/scala/data_structures/cracking_the_coding_interview/string_1_2.scala
+++ src/main/scala/data_structures/cracking_the_coding_interview/string_1_2.scala
@@ -13,42 +13,48 @@ object string_1_2 extends App{
     // edge case:
       //if string size = 2, reverse the chars and send back
       //if string is 1 char, return the char
-      // function is going to inupt string
+      //if string is empty
+    //function is going to input string
+
 
   val testset_01 = "abcdefg"
-  println("input string -->[" +testset_01+"] output string  [\t"+ reverse_string(testset_01))
+  println("\ninput string -->[" +testset_01+"] output string -->"+ reverse_string(testset_01))
+
+  val testset_02 = "ab"
+  println("\ninput string -->[" +testset_02+"] output string -->"+ reverse_string(testset_02))
+
+  val testset_03 = "a"
+  println("\ninput string -->[" +testset_03+"] output string -->"+ reverse_string(testset_03))
+
+  val testset_04 = ""
+  println("\ninput string -->[" +testset_04+"] output string -->"+ reverse_string(testset_04))
+
+  val testset_05 = "asdf;jk23r90sa()#$*"
+  println("\ninput string -->[" +testset_05+"] output string -->"+ reverse_string(testset_05))
+
+
 
   println("----")
 
   def reverse_string(string_val: String): String ={
-
-    println("=========input string======: ")
-
     val string_size=string_val.length
-    println("string size "+string_size)
     //edge case 1
+
+    if (string_size==0 || string_val==null){
+      return string_val
+    }
     if (string_size==1){
-      println("size 1: "+string_val)
       return string_val
     }
     if (string_size==2){
-      println("size 2: "+string_val.slice(1,2)+string_val.slice(0,1))
-
       return string_val.slice(1,2)+string_val.slice(0,1)
     }
     val left_index=0
     val mid_index=string_size/2
     val right_index=string_size
 
-    println("lengths : ["+left_index+"] ["+mid_index+"] ["+right_index+"]")
-
     val left_string=reverse_string(string_val.slice(left_index,mid_index))
     val right_string=reverse_string(string_val.slice(mid_index,right_index))
-
-    println("left string: "+left_string)
-    println("right string: "+right_string)
-
-    println("===>>>>>>==")
 
     right_string+left_string
   }
