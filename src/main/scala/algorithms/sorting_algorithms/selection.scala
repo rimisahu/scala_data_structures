1f66d3b467203579858dff3a9bba8146efe1a03f
rimisahu <rimi.m.sahu@gmail.com>
2019-11-23 13:49:21 -0500

    selection sort. playing around with hashes

---
+++ src/main/scala/algorithms/sorting_algorithms/selection.scala
@@ -0,0 +1,56 @@
+package algorithms.sorting_algorithms
+
+object selection extends App{
+
+
+    /*
+    * Take index 0 as pivot
+    * Find the min vaule in array compated to pivot  
+    * Swap min value with pivot
+    * Move pivot counter to 1 
+    * Find min after that Swap
+    * Rinse <-> Repeat
+    * */
+
+    val testset_01 = Array(4,5,6,7,1,2,3)
+    println("\ninput string -->[testset_01] output string -->")
+    val result01=run_selection(testset_01)
+    result01.foreach(println)
+
+  val testset_02 = Array(234, 7, 24, 32)
+  println("\ninput string -->[testset_02] output string -->")
+  val result02=run_selection(testset_02)
+  result02.foreach(println)
+
+  val testset_03 = Array(1)
+  println("\ninput string -->[testset_03] output string -->")
+  val result03=run_selection(testset_03)
+  result03.foreach(println)
+
+    def run_selection(Arr: Array[Int]): Array[Int] ={
+
+      var final_arr=Arr
+      val arr_size=final_arr.size
+
+      for (i <- 0 to arr_size-1){
+        var pivot=i
+        for (j <- i+1 to arr_size-1){
+          if (final_arr(pivot)>final_arr(j)){
+            pivot=j
+          }
+        }
+        final_arr=swap(final_arr,i,pivot)
+      }
+
+      final_arr
+    }
+
+    def swap(inputArray:Array[Int],left_index:Int,right_index:Int): Array[Int] ={
+      val temp=inputArray(left_index)
+      inputArray(left_index)=inputArray(right_index)
+      inputArray(right_index)=temp
+      inputArray
+    }
+
+
+}
---
+++ src/main/scala/data_structures/hash/hash_example1.scala
@@ -0,0 +1,13 @@
+package data_structures.hash
+
+object hash_example1 {
+
+  val first_map=Map(88 -> "rimi", 87 -> "vivek", 86 -> "happy")
+
+  def main(args: Array[String]): Unit ={
+
+    println(first_map(88))
+
+  }
+
+}
