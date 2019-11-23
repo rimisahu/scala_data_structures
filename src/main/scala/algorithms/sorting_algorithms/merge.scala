ca1bc429d7a0cfa707ec259bf467aff340955750
rimisahu <rimi.m.sahu@gmail.com>
2019-11-23 15:59:18 -0500

    merge sort complete! woo!

--- src/main/scala/algorithms/sorting_algorithms/merge.scala
+++ src/main/scala/algorithms/sorting_algorithms/merge.scala
@@ -18,44 +18,62 @@ object merge extends App{
 
 
 
-//  val testset_01 = Array(4, 5, 6, 7, 1, 2, 3)
-//  println("\ninput string -->[testset_01] output string -->")
-//  val result01 = run_merge_sort(testset_01)
-//  result01.foreach(println)
-//
-//  val testset_02 = Array(234, 7, 24, 32)
-//  println("\ninput string -->[testset_02] output string -->")
-//  val result02 = run_merge_sort(testset_02)
-//  result02.foreach(println)
-//
-//  val testset_03 = Array(1)
-//  println("\ninput string -->[testset_03] output string -->")
-//  val result03 = run_merge_sort(testset_03)
-//  result03.foreach(println)
+  val testset_01 = Array(4, 5, 6, 7, 1, 2, 3)
+  println("\ninput string -->[testset_01] output string -->")
+  val result01 = run_merge_sort(testset_01)
+  result01.foreach(println)
+
+  val testset_02 = Array(234, 7, 24, 32)
+  println("\ninput string -->[testset_02] output string -->")
+  val result02 = run_merge_sort(testset_02)
+  result02.foreach(println)
+
+  val testset_03 = Array(1)
+  println("\ninput string -->[testset_03] output string -->")
+  val result03 = run_merge_sort(testset_03)
+  result03.foreach(println)
+
+  val testset_04= Array(11,4)
+  println("\ninput string -->[testset_04] output string -->")
+  val result04 = run_merge_sort(testset_04)
+  result04.foreach(println)
 
 
   val testset_11 = Array(4, 5, 6, 7, 1, 2, 3)
   val testset_11_l=Array(1, 2, 3, 4)
   val testset_11_r=Array(5, 6, 7)
   println("\ninput string -->[testset_11] output string -->")
-  val result01 = merge(testset_11_l,testset_11_r,testset_11)
-  result01.foreach(println)
+  val result11 = merge(testset_11_l,testset_11_r,testset_11)
+  result11.foreach(println)
 
   val testset_12 = Array(234, 7, 24, 32)
   val testset_12_l = Array(7)
   val testset_12_r = Array(24, 32,234)
   println("\ninput string -->[testset_12] output string -->")
-  val result02 = merge(testset_12_l,testset_12_r,testset_12)
-  result02.foreach(println)
+  val result12 = merge(testset_12_l,testset_12_r,testset_12)
+  result12.foreach(println)
 
 
 
 
   def run_merge_sort(Arr: Array[Int]): Array[Int] = {
 
+    val arr_len=Arr.size
+    //instantiate left and right arrays
+    var left_arr: Array[Int]= Array()
+    var right_arr: Array[Int]=Array()
 
-    Arr
+    if (arr_len<2){
+      return Arr
+    } else {
 
+      val mid_index = arr_len/2
+      left_arr=run_merge_sort(Arr.slice(0,mid_index))
+      right_arr=run_merge_sort(Arr.slice(mid_index,arr_len))
+
+    }
+
+    merge(left_arr,right_arr,Arr)
 
   }
 
